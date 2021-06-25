9c5115dc8ec043fff331102fe378d7add4d581fe
Connor <connorjrobertson@hotmail.co.uk>
2021-06-25 18:11:44 +0100

    Created a guest_pays_entry_fee() function that removes the entry fee price of a specific room, ran and passed test for this

--- src/guest.py
+++ src/guest.py
@@ -4,3 +4,6 @@ class Guest:
     def __init__(self,name, wallet):
         self.name = name
         self.wallet = wallet
+
+    def guest_pay_entry_fee(self, room):
+        self.wallet -= room.entry_fee
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -14,3 +14,10 @@ class TestGuest(unittest.TestCase):
 
     def test_guest_has_wallet(self):
         self.assertEqual(15.00, self.guest_1.wallet)
+
+    def test_guest_pays_entry_fee(self):
+        self.room_1 = Room(1, 6, 4.00)
+        self.guest_1.guest_pay_entry_fee(self.room_1)
+        self.assertEqual(11.00, self.guest_1.wallet)
+        
+        
