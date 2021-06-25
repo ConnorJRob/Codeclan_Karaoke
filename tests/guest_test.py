5f58a8cade07c76c36c4731e9f782d5855f4b867
Connor <connorjrobertson@hotmail.co.uk>
2021-06-25 15:23:07 +0100

    Added a test to Room_test that checks the soundtrack list starts off empty, also added a test ot the song_test file to check that the name property is working correctly - both tests passed

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,4 +4,10 @@ from src.guest import *
 from src.song import *
 
 class TestGuest(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.guest_1 = Guest("Loki")
+
+    def test_song_has_name(self):
+        #This test checks that given the Guest object created above, the guest.name property has been correctly setup matching "Loki"
+        self.assertEqual("Loki", self.guest_1.name)
