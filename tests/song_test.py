f0d1c98c312308404c7dc95774852d4f0f9f8cdc
Connor <connorjrobertson@hotmail.co.uk>
2021-06-25 15:19:04 +0100

    Initial tests for song are complete, for now this is simply test_song_has_name

--- tests/song_test.py
+++ tests/song_test.py
@@ -4,4 +4,10 @@ from src.guest import *
 from src.song import *
 
 class TestSong(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.song_1 = Song("The Eye of the Tiger")
+
+    def test_song_has_name(self):
+        #This test checks that given the Room object created above, the room.name property has been correctly setup matching "The Eye of the Tiger"
+        self.assertEqual("The Eye of the Tiger", self.song_1.song_name)
