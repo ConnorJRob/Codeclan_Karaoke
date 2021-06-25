f639b3a858bc9ba7ba170f502f7c8a47554da441
Connor <connorjrobertson@hotmail.co.uk>
2021-06-25 15:03:26 +0100

    All files and directories created

---
+++ tests/guest_test.py
@@ -0,0 +1,7 @@
+import unittest
+from src.room import *
+from src.guest import *
+from src.song import *
+
+class TestGuest(unittest.TestCase):
+    pass
---
+++ tests/room_test.py
@@ -0,0 +1,7 @@
+import unittest
+from src.room import *
+from src.guest import *
+from src.song import *
+
+class TestRoom(unittest.TestCase):
+    pass
